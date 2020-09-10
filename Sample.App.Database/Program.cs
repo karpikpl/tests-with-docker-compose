5d4f671e039e37932a3c4a1b66b5e40c0cd2e257
Yann Normand <yann.normand@gmail.com>
2020-09-11 09:15:40 +1000

    use DbUp and retry until SQL Server is ready

--- Sample.App.Database/Program.cs
+++ Sample.App.Database/Program.cs
@@ -1,21 +1,65 @@
-using Beef.Database.Core;
-using System.Threading.Tasks;
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading;
+using DbUp;
 
 namespace Sample.App.Database
 {
-    /// <summary>
-    /// Represents the <b>database utilities</b> program (capability).
-    /// </summary>
-    public static class Program
+    class Program
     {
-        /// <summary>
-        /// Main startup.
-        /// </summary>
-        /// <param name="args">The startup arguments.</param>
-        /// <returns>The status code whereby zero indicates success.</returns>
-        static Task<int> Main(string[] args)
+        static void Main(string[] args)
         {
-            return DatabaseConsoleWrapper.Create("Data Source=.;Initial Catalog=SampleDB;Integrated Security=True", "Sample", "App").RunAsync(args);
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine($"Expecting a single connection string argument but received {args.Length} argument(s).");
+                Environment.Exit(-1);
+                return;
+            }
+
+            var connectionString = args[0];
+
+            ExecuteWithRetry(
+                () => EnsureDatabase.For.SqlDatabase(connectionString),
+                retryCondition: ex => ex is SqlException sqlEx && (sqlEx.Number == 17187 || sqlEx.Number == 18456),
+                onRetry: (ex, attempts, maxTimes, delay) => Console.WriteLine($"SQL Server is not ready. Retry {attempts}/{maxTimes} in {delay}ms"),
+                maxAttempts: 10,
+                delay: 1000);
+
+
+            var dbup = DeployChanges.To
+                .SqlDatabase(connectionString)
+                .LogToConsole()
+                .LogScriptOutput()
+                .WithExecutionTimeout(TimeSpan.FromSeconds(30))
+                .WithScriptsFromFileSystem("./scripts")
+                .Build();
+
+
+            var result = dbup.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.Error.WriteLine(result.Error);
+                Environment.Exit(-1);
+            }
+        }
+
+        static void ExecuteWithRetry(Action action, Func<Exception, bool> retryCondition, Action<Exception, int, int, int> onRetry, int maxAttempts, int delay)
+        {
+            var attempts = 0;
+            while (attempts <= maxAttempts)
+            {
+                attempts++;
+                try
+                {
+                    action();
+                }
+                catch (Exception ex) when (attempts < maxAttempts && retryCondition(ex))
+                {
+                    onRetry(ex, attempts, maxAttempts, delay);
+                    Thread.Sleep(delay);
+                }
+            }
         }
     }
 }
